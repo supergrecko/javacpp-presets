3d6756c9d6012b1d18cd5a8ffd00988e8ceff9c4
Mats Larsen <me@supergrecko.com>
2021-04-17 20:31:50 +0200

    Fix incorrect install path and use absolute paths for files/directories

--- llvm/cppbuild.sh
+++ llvm/cppbuild.sh
@@ -7,20 +7,27 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
+# To allow the building to happen on another system drive than we're currently
+# on, we use these variables to address absolute paths to files and directories.
+
+# BUILD_ROOT is the directory where the building happens
+BUILD_ROOT="${BUILD_DIR:-$(pwd)}"
+# PRESET_SOURCES_DIR is the directory where the javacpp-presets repository was cloned into
+PRESET_SOURCES_DIR=$(pwd)/../
+# INSTALL_PATH is the directory where the libraries, headers and executables are stored. /cppbuild/$PLATFORM
+INSTALL_PATH=$(pwd)/$PLATFORM
+mkdir -p $BUILD_ROOT
+cd $BUILD_ROOT
+
 LLVM_VERSION=12.0.0
 download https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/llvm-project-$LLVM_VERSION.src.tar.xz llvm-project-$LLVM_VERSION.src.tar.xz
-INSTALL_PATH=`pwd`
-# Enter a custom build directory if necessary
-BUILD_DIR="${BUILD_DIR:-$(pwd)}"
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
 
 mkdir -p $PLATFORM
 cd $PLATFORM
 echo "Decompressing archives... (ignore any symlink errors)"
-tar --totals -xf $INSTALL_PATH/llvm-project-$LLVM_VERSION.src.tar.xz || true
+tar --totals -xf $BUILD_ROOT/llvm-project-$LLVM_VERSION.src.tar.xz || true
 cd llvm-project-$LLVM_VERSION.src
-patch -Np1 < $INSTALL_PATH/../llvm.patch
+patch -Np1 < $PRESET_SOURCES_DIR/llvm.patch
 sedinplace '/find_package(Git/d' llvm/cmake/modules/AddLLVM.cmake llvm/cmake/modules/VersionFromVCS.cmake
 sedinplace '/Generating libLLVM is not supported on MSVC/d' llvm/tools/llvm-shlib/CMakeLists.txt
 mkdir -p build
@@ -35,7 +42,7 @@ case $PLATFORM in
         $CMAKE -DLLVM_CCACHE_BUILD=ON -DCMAKE_EXE_LINKER_FLAGS='-Wl,-rpath,$ORIGIN/' -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         make -j $MAKEJ
         make install > /dev/null
-        cp lib/LLVMPolly.so ../../lib/libLLVMPolly.so
+        cp lib/LLVMPolly.so $INSTALL_PATH/lib/libLLVMPolly.so
         ;;
     linux-x86_64)
         export CC="gcc -m64"
@@ -43,7 +50,7 @@ case $PLATFORM in
         $CMAKE -DLLVM_CCACHE_BUILD=ON -DCMAKE_EXE_LINKER_FLAGS='-Wl,-rpath,$ORIGIN/' -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         make -j $MAKEJ
         make install > /dev/null
-        cp lib/LLVMPolly.so ../../lib/libLLVMPolly.so
+        cp lib/LLVMPolly.so $INSTALL_PATH/lib/libLLVMPolly.so
         ;;
     linux-armhf)
         mkdir -p ../tblgen
@@ -55,7 +62,7 @@ case $PLATFORM in
         CC="arm-linux-gnueabihf-gcc" CXX="arm-linux-gnueabihf-g++" $CMAKE -DCMAKE_EXE_LINKER_FLAGS="-ldl" -DCMAKE_SHARED_LINKER_FLAGS="-ldl" -DLLVM_CCACHE_BUILD=ON -DCMAKE_CROSSCOMPILING=True -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON -DCMAKE_EXE_LINKER_FLAGS='-Wl,-rpath,$ORIGIN/' -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_TABLEGEN="$TBLGEN/bin/llvm-tblgen" -DCLANG_TABLEGEN="$TBLGEN/bin/clang-tblgen" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_HOST_TRIPLE=arm-unknown-linux-gnueabihf -DLLVM_DEFAULT_TARGET_TRIPLE=arm-unknown-linux-gnueabihf -DLLVM_TARGET_ARCH=ARM -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         make -j $MAKEJ
         make install > /dev/null
-        cp lib/LLVMPolly.so ../../lib/libLLVMPolly.so
+        cp lib/LLVMPolly.so $INSTALL_PATH/lib/libLLVMPolly.so
         ;;
     linux-arm64)
         mkdir -p ../tblgen
@@ -67,7 +74,7 @@ case $PLATFORM in
         CC="aarch64-linux-gnu-gcc" CXX="aarch64-linux-gnu-g++" $CMAKE -DLLVM_CCACHE_BUILD=ON -DCMAKE_CROSSCOMPILING=True -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON -DCMAKE_EXE_LINKER_FLAGS='-Wl,-rpath,$ORIGIN/' -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_TABLEGEN="$TBLGEN/bin/llvm-tblgen" -DCLANG_TABLEGEN="$TBLGEN/bin/clang-tblgen" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_HOST_TRIPLE=aarch64-unknown-linux-gnu -DLLVM_DEFAULT_TARGET_TRIPLE=aarch64-unknown-linux-gnu -DLLVM_TARGET_ARCH=AArch64 -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         make -j $MAKEJ
         make install > /dev/null
-        cp lib/LLVMPolly.so ../../lib/libLLVMPolly.so
+        cp lib/LLVMPolly.so $INSTALL_PATH/lib/libLLVMPolly.so
         ;;
     linux-ppc64le)
         mkdir -p ../tblgen
@@ -79,13 +86,13 @@ case $PLATFORM in
         CC="powerpc64le-linux-gnu-gcc" CXX="powerpc64le-linux-gnu-g++" $CMAKE -DLLVM_CCACHE_BUILD=ON -DCMAKE_CROSSCOMPILING=True -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON -DCMAKE_EXE_LINKER_FLAGS='-Wl,-rpath,$ORIGIN/' -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_TABLEGEN="$TBLGEN/bin/llvm-tblgen" -DCLANG_TABLEGEN="$TBLGEN/bin/clang-tblgen" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_HOST_TRIPLE=powerpc64le-unknown-linux-gnu -DLLVM_DEFAULT_TARGET_TRIPLE=powerpc64le-unknown-linux-gnu -DLLVM_TARGET_ARCH=PowerPC -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         make -j $MAKEJ
         make install > /dev/null
-        cp lib/LLVMPolly.so ../../lib/libLLVMPolly.so
+        cp lib/LLVMPolly.so $INSTALL_PATH/lib/libLLVMPolly.so
         ;;
     macosx-*)
         $CMAKE -DLLVM_CCACHE_BUILD=ON -DCMAKE_EXE_LINKER_FLAGS='-Wl,-rpath,@loader_path/' -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         make -j $MAKEJ
         make install > /dev/null
-        cp lib/LLVMPolly.so ../../lib/libLLVMPolly.so
+        cp lib/LLVMPolly.so $INSTALL_PATH/lib/libLLVMPolly.so
         ;;
     windows-x86)
         export CC="cl.exe"
@@ -99,11 +106,10 @@ case $PLATFORM in
         $CMAKE -G "Ninja" -DLLVM_USE_CRT_RELEASE=MD -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_BUILD_LLVM_C_DYLIB=OFF -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_DIA_SDK=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DPYTHON_EXECUTABLE="$(where python.exe | head -1)" -DLLVM_POLLY_LINK_INTO_TOOLS=ON -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         ninja -j $MAKEJ
         ninja install
-        cd ../../lib
+        cd $INSTALL_PATH/lib
         [ -f LLVM.lib ] || lib.exe /OUT:LLVM.lib LLVM*.lib Polly*.lib
         [ -f clang.lib ] || lib.exe /OUT:clang.lib clang*.lib
-        [ -f LTO.lib ] || cp ../llvm-$LLVM_VERSION.src/build/lib/LTO.lib .
-        cd ../llvm-project-$LLVM_VERSION.src/build
+        [ -f LTO.lib ] || cp $BUILD_ROOT/llvm-$LLVM_VERSION.src/build/lib/LTO.lib .
         ;;
     windows-x86_64)
         export CC="cl.exe"
@@ -117,11 +123,10 @@ case $PLATFORM in
         $CMAKE -G "Ninja" -DLLVM_USE_CRT_RELEASE=MD -DCMAKE_INSTALL_PREFIX=../.. -DLLVM_BUILD_LLVM_C_DYLIB=OFF -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=all -DLLVM_ENABLE_DIA_SDK=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_INCLUDE_TESTS=OFF -DPYTHON_EXECUTABLE="$(where python.exe | head -1)" -DLLVM_POLLY_LINK_INTO_TOOLS=ON -DLLVM_ENABLE_PROJECTS="$PROJECTS" ../llvm
         ninja -j $MAKEJ
         ninja install
-        cd ../../lib
+        cd $INSTALL_PATH/lib
         [ -f LLVM.lib ] || lib.exe /OUT:LLVM.lib LLVM*.lib Polly*.lib
         [ -f clang.lib ] || lib.exe /OUT:clang.lib clang*.lib
-        [ -f LTO.lib ] || cp ../llvm-$LLVM_VERSION.src/build/lib/LTO.lib .
-        cd ../llvm-project-$LLVM_VERSION.src/build
+        [ -f LTO.lib ] || cp $BUILD_ROOT/llvm-$LLVM_VERSION.src/build/lib/LTO.lib .
         ;;
     *)
         echo "Error: Platform \"$PLATFORM\" is not supported"
@@ -129,4 +134,4 @@ case $PLATFORM in
         ;;
 esac
 
-cd ../..
+cd $BUILD_ROOT
